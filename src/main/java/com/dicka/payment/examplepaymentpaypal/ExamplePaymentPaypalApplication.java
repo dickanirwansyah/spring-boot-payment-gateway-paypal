217fee6b4d2d48f7f3af6806ce8b24bf6c6eeba6
dickanirwansyah <dickanirwansyah@gmail.com>
2020-06-04 21:10:37 +0800

    payment gateway using paypall

---
+++ src/main/java/com/dicka/payment/examplepaymentpaypal/ExamplePaymentPaypalApplication.java
@@ -0,0 +1,13 @@
+package com.dicka.payment.examplepaymentpaypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplePaymentPaypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplePaymentPaypalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dicka/payment/examplepaymentpaypal/ExamplePaymentPaypalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicka.payment.examplepaymentpaypal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplePaymentPaypalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
